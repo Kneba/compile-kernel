c1db40cbee058278ec016d5804f8c2fc8a4a9262
Kneba <abenkenary3@gmail.com>
2022-12-12 08:09:14 +0700

    build: update [ci skip]

--- build.sh
+++ build.sh
@@ -3,6 +3,22 @@
 # Copyright (C) 2022 Kneba <abenkenary3@gmail.com>
 #
 
+# Function to show an informational message
+msg() {
+	echo
+    echo -e "\e[1;32m$*\e[0m"
+    echo
+}
+
+err() {
+    echo -e "\e[1;41m$*\e[0m"
+}
+
+cdir() {
+	cd "$1" 2>/dev/null || \
+		err "The directory $1 doesn't exists !"
+}
+
 # Main
 MainPath="$(pwd)"
 MainClangPath="${MainPath}/clang"
@@ -13,26 +29,37 @@ GCCbPath="${MainPath}/GCC32"
 MainZipGCCaPath="${MainPath}/GCC64-zip"
 MainZipGCCbPath="${MainPath}/GCC32-zip"
 
-git clone https://$TOKEN@github.com/Kneba/android_kernel_asus_sdm660 -b cat $DEVICE_CODENAME
+# Identity
+VERSION=13
+KERNELNAME=TheOneMemory
+CODENAME=Hayzel
+VARIANT=EAS
 
+# Show manufacturer info
+MANUFACTURERINFO="ASUSTek Computer Inc."
+
+# Clone Kernel Source
+git clone --depth=1 https://$USERNAME:$TOKEN@github.com/Kneba/platform_kernel_asus_sdm660 -b eas $DEVICE_CODENAME
+
+# Clone AOSP Clang
 ClangPath=${MainClangZipPath}
 [[ "$(pwd)" != "${MainPath}" ]] && cd "${MainPath}"
 mkdir $ClangPath
 rm -rf $ClangPath/*
-wget -q  https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/master/clang-r445002.tar.gz -O "clang-r445002.tar.gz"
-tar -xf clang-r445002.tar.gz -C $ClangPath
+wget -q  https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/master/clang-r416183b1.tar.gz -O "clang-r458507.tar.gz"
+tar -xf clang-r416183b1.tar.gz -C $ClangPath
 
+# Clone GCC
 mkdir $GCCaPath
 mkdir $GCCbPath
-wget -q https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/+archive/refs/tags/android-12.0.0_r32.tar.gz -O "gcc64.tar.gz"
+wget -q https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/+archive/refs/tags/android-12.1.0_r16.tar.gz -O "gcc64.tar.gz"
 tar -xf gcc64.tar.gz -C $GCCaPath
-wget -q https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/+archive/refs/tags/android-12.0.0_r32.tar.gz -O "gcc32.tar.gz"
+wget -q https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/+archive/refs/tags/android-12.1.0_r16.tar.gz -O "gcc32.tar.gz"
 tar -xf gcc32.tar.gz -C $GCCbPath
 
-# Prepare 1
+# Prepare
 KERNEL_ROOTDIR=$(pwd)/$DEVICE_CODENAME # IMPORTANT ! Fill with your kernel source root directory.
-export LD="ld.lld"
-export KERNELNAME=TOM
+export LD=ld.lld
 export KBUILD_BUILD_USER=queen # Change with your own name or else.
 IMAGE=$(pwd)/$DEVICE_CODENAME/out/arch/arm64/boot/Image.gz-dtb
 CLANG_VER="$("$ClangPath"/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
@@ -40,19 +67,20 @@ LLD_VER="$("$ClangPath"/bin/ld.lld --version | head -n 1)"
 export KBUILD_COMPILER_STRING="$CLANG_VER with $LLD_VER"
 DATE=$(date +"%F-%S")
 START=$(date +"%s")
-PATH=${ClangPath}/bin:${GCCaPath}/bin:${GCCbPath}/bin:${PATH}
+
+# Java
+command -v java > /dev/null 2>&1
 
 # Telegram
 export BOT_MSG_URL="https://api.telegram.org/bot$TG_TOKEN/sendMessage"
 
+# Telegram messaging
 tg_post_msg() {
   curl -s -X POST "$BOT_MSG_URL" -d chat_id="$TG_CHAT_ID" \
-  -d "disable_web_page_preview=true" \
-  -d "parse_mode=html" \
-  -d text="$1"
-
+    -d "disable_web_page_preview=true" \
+    -d "parse_mode=html" \
+    -d text="$1"
 }
-
 # Compile
 compile(){
 cd ${KERNEL_ROOTDIR}
@@ -61,6 +89,7 @@ export COMMIT_HEAD=$(git log --oneline -1)
 make -j$(nproc) O=out ARCH=arm64 $KERNEL_DEFCONFIG
 make -j$(nproc) ARCH=arm64 O=out \
     LD_LIBRARY_PATH="${ClangPath}/lib64:${LD_LIBRARY_PATH}" \
+    PATH=$ClangPath/bin:$GCCaPath/bin:$GCCbPath/bin:/usr/bin:${PATH} \
     CC=${ClangPath}/bin/clang \
     NM=${ClangPath}/bin/llvm-nm \
     CXX=${ClangPath}/bin/clang++ \
@@ -81,33 +110,58 @@ make -j$(nproc) ARCH=arm64 O=out \
 	finerr
 	exit 1
    fi
-  git clone --depth=1 $ANYKERNEL -b tom AnyKernel3
-	cp $IMAGE AnyKernel3
+   git clone $ANYKERNEL -b eas-compile AnyKernel
+	cp $IMAGE AnyKernel
 }
-# Push kernel to channel
+# Push kernel to telegram
 function push() {
-    cd AnyKernel3
-    ZIP=$(echo *.zip)
-    curl -F document=@$ZIP "https://api.telegram.org/bot$TG_TOKEN/sendDocument" \
+    cd AnyKernel
+    curl -F document="@$ZIP_FINAL.zip" "https://api.telegram.org/bot$TG_TOKEN/sendDocument" \
         -F chat_id="$TG_CHAT_ID" \
         -F "disable_web_page_preview=true" \
         -F "parse_mode=html" \
-        -F caption="Compile took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>$DEVICE_CODENAME</b> | <b>${KBUILD_COMPILER_STRING}</b>"
+        -F caption="🔐<b>Build Done</b>
+        - <code>$((DIFF / 60)) minute(s) $((DIFF % 60)) second(s)... </code>
+
+        <b>📅 Build Date: </b>
+        -<code>$DATE</code>
+
+        <b>🐧 Linux Version: </b>
+        -<code>4.4.302</code>
+
+         <b>💿 Compiler: </b>
+        -<code>$KBUILD_COMPILER_STRING</code>
+
+        <b>📱 Device: </b>
+        -<code>$DEVICE_CODENAME($MANUFACTURERINFO)</code>
+
+        <b>🆑 Changelog: </b>
+        - <code>$COMMIT_HEAD</code>
+        <b></b>
+        #ElectroWizard #CAF #EAS"
 }
-# Fin Error
+# Find Error
 function finerr() {
     curl -s -X POST "https://api.telegram.org/bot$TG_TOKEN/sendMessage" \
         -d chat_id="$TG_CHAT_ID" \
         -d "disable_web_page_preview=true" \
         -d "parse_mode=markdown" \
-        -d text="Aku sudah lelah compile kernel,Dan aku memilih menyerah ;(,tolong beri aku motivasi"
+        -d text="❌ Tetap menyerah...Pasti bisa!!!"
     exit 1
 }
-
 # Zipping
 function zipping() {
-    cd AnyKernel3 || exit 1
-    zip -r9 [NLV][$KERNELNAME]-HMP-4.4.302-$DATE.zip *
+    cd AnyKernel || exit 1
+    zip -r9 [$VERSION]$KERNELNAME-$CODENAME-$VARIANT-"$DATE" . -x ".git*" -x "README.md" -x "*.zip"
+
+    ZIP_FINAL="[$VERSION]$KERNELNAME-$CODENAME-$VARIANT-$DATE"
+
+    msg "|| Signing Zip ||"
+    tg_post_msg "<code>🔑 Signing Zip file with AOSP keys..</code>"
+
+    curl -sLo zipsigner-3.0.jar https://github.com/Magisk-Modules-Repo/zipsigner/raw/master/bin/zipsigner-3.0-dexed.jar
+    java -jar zipsigner-3.0.jar "$ZIP_FINAL".zip "$ZIP_FINAL"-signed.zip
+    ZIP_FINAL="$ZIP_FINAL-signed"
     cd ..
 }
 compile
