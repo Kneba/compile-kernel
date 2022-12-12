ff2e28b89ce010a863dec8db00e8a4713ff9a4b8
Kneba <abenkenary3@gmail.com>
2022-12-12 08:26:31 +0700

    r450784d

--- build.sh
+++ build.sh
@@ -46,8 +46,8 @@ ClangPath=${MainClangZipPath}
 [[ "$(pwd)" != "${MainPath}" ]] && cd "${MainPath}"
 mkdir $ClangPath
 rm -rf $ClangPath/*
-wget -q  https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/master/clang-r416183b1.tar.gz -O "clang-r416183b1.tar.gz"
-tar -xf clang-r416183b1.tar.gz -C $ClangPath
+wget -q  https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/master/clang-r450784d.tar.gz -O "clang-r450784d.tar.gz"
+tar -xf clang-r450784d.tar.gz -C $ClangPath
 
 # Clone GCC
 mkdir $GCCaPath
