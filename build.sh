9bc670c9125408a7c6e8f6e18901af3d6f72807e
Kneba <abenkenary3@gmail.com>
2023-06-11 22:29:13 +0700

    ci: clo-hmp-oc

--- build.sh
+++ build.sh
@@ -34,14 +34,14 @@ GCCbPath="${MainGCCbPath}"
 # Identity
 VERSION=9x13
 KERNELNAME=TheOneMemory
-CODENAME=Hayzel
+CODENAME=Onyx
 VARIANT=HMP
 
 # Show manufacturer info
 MANUFACTURERINFO="ASUSTek Computer Inc."
 
 # Clone Kernel Source
-git clone --depth=1 https://$USERNAME:$TOKEN@github.com/Kneba/platform_kernel_asus_sdm660 -b clo-hmp kernel
+git clone --depth=1 https://$USERNAME:$TOKEN@github.com/Kneba/platform_kernel_asus_sdm660 -b clo-hmp-oc kernel
 
 # Clone Snapdragon Clang
 ClangPath=${MainClangPath}
