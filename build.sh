16eb071fdfd40909d2c1d4502fb098723b5449df
Meisie Widuri <ewprjkt@proton.me>
2023-07-04 17:08:48 +0700

    onyx-eas

--- build.sh
+++ build.sh
@@ -34,14 +34,14 @@ GCCbPath="${MainGCCbPath}"
 # Identity
 VERSION=9x13
 KERNELNAME=TheOneMemory
-CODENAME=Hayzel
+CODENAME=Onyx
 VARIANT=EAS-CLO
 
 # Show manufacturer info
 MANUFACTURERINFO="ASUSTek Computer Inc."
 
 # Clone Kernel Source
-git clone --depth=1 https://$USERNAME:$TOKEN@github.com/Tiktodz/android_kernel_asus_sdm636 -b caf-eas kernel
+git clone --depth=1 https://$USERNAME:$TOKEN@github.com/Tiktodz/android_kernel_asus_sdm636 -b caf-eas-oc kernel
 
 # Clone Snapdragon Clang
 ClangPath=${MainClangPath}
