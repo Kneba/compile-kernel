e2709f232d23a32a4b4a4a972b9749b3ff77dfa1
fakedotkit <ewprjkt@proton.me>
2023-12-29 10:51:56 +0000

    test

--- build.sh
+++ build.sh
@@ -1,168 +1,115 @@
 #!/usr/bin/env bash
 #
-# Copyright (C) 2023 Kneba <abenkenary3@gmail.com>
+# Copyright (C) 2022  <abenkenary3@gmail.com>
 #
-
-#
-# Function to show an informational message
-#
-
-msg() {
-	echo
-    echo -e "\e[1;32m$*\e[0m"
-    echo
-}
-
-err() {
-    echo -e "\e[1;41m$*\e[0m"
-}
-
-cdir() {
-	cd "$1" 2>/dev/null || \
-		err "The directory $1 doesn't exists !"
-}
 
 # Main
 MainPath="$(pwd)"
 MainClangPath="${MainPath}/clang"
-ClangPath="${MainClangPath}"
-#MainGCCaPath="${MainPath}/GCC64"
-#MainGCCbPath="${MainPath}/GCC32"
-#GCCaPath="${MainGCCaPath}"
-#GCCbPath="${MainGCCbPath}"
+MainClangZipPath="${MainPath}/clang-zip"
+ClangPath="${MainClangZipPath}"
+GCCaPath="${MainPath}/GCC64"
+GCCbPath="${MainPath}/GCC32"
+MainZipGCCaPath="${MainPath}/GCC64-zip"
+MainZipGCCbPath="${MainPath}/GCC32-zip"
 
-# Identity
-VERSION=4.19.300
-KERNELNAME=TheOneMemory
-CODENAME=Hayzel
-VARIANT=EAS
-BASE=CLO
+git clone --recursive https://$USERNAME:$TOKEN@github.com/Tiktodz/android_kernel_asus-sdm660-4.19 kernel
 
-# Show manufacturer info
-MANUFACTURERINFO="ASUSTek Computer Inc."
-
-# Changelogs
-CL_URL="https://github.com/Tiktodz/android_kernel_asus_sdm660-4.19/commits/r2/s"
-
-# Clone Kernel Source
-git clone --recursive https://$USERNAME:$TOKEN@github.com/Tiktodz/android_kernel_asus_sdm660-4.19 kernel
-
-# Clone Snapdragon Clang
-ClangPath=${MainClangPath}
+ClangPath=${MainClangZipPath}
 [[ "$(pwd)" != "${MainPath}" ]] && cd "${MainPath}"
 mkdir $ClangPath
 rm -rf $ClangPath/*
-msg "|| Cloning sdclang toolchain ||"
-git clone --depth=1 https://gitlab.com/varunhardgamer/trb_clang -b 17 $ClangPath
+wget -q  https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/master/clang-r487747c.tar.gz -O "clang-r487747c.tar.gz"
+tar -xf clang-r487747c.tar.gz -C $ClangPath
 
-# Clone GCC
-#mkdir $GCCaPath
-#mkdir $GCCbPath
+mkdir $GCCaPath
+mkdir $GCCbPath
+wget -q https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/+archive/refs/tags/android-12.1.0_r16.tar.gz -O "gcc64.tar.gz"
+tar -xf gcc64.tar.gz -C $GCCaPath
+wget -q https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/+archive/refs/tags/android-12.1.0_r16.tar.gz -O "gcc32.tar.gz"
+tar -xf gcc32.tar.gz -C $GCCbPath
 
-#msg "|| Cloning GCC 4.9.x toolchain ||"
-#git clone --depth=1 https://github.com/Kneba/aarch64-linux-android-4.9 $GCCaPath
-#git clone --depth=1 https://github.com/Kneba/arm-linux-androideabi-4.9 $GCCbPath
-
-# Prepare
+# Prepared
 KERNEL_ROOTDIR=$(pwd)/kernel # IMPORTANT ! Fill with your kernel source root directory.
-export KBUILD_BUILD_USER=queen # Change with your own name or else.
 export LD=ld.lld
-export LD_LIBRARY_PATH=$ClangPath/lib
+export KERNELNAME=KnightWalker
+export KBUILD_BUILD_USER=rj # Change with your own name or else.
 IMAGE=$(pwd)/kernel/out/arch/arm64/boot/Image.gz-dtb
-ClangMoreStrings="AR=llvm-ar NM=llvm-nm AS=llvm-as STRIP=llvm-strip OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump READELF=llvm-readelf HOSTAR=llvm-ar HOSTAS=llvm-as LD_LIBRARY_PATH=$ClangPath/lib LD=ld.lld HOSTLD=ld.lld"
-export TZ=Asia/Jakarta
-DATE=$(date +"%Y-%m-%d")
+CLANG_VER="$("$ClangPath"/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
+#LLD_VER="$("$ClangPath"/bin/ld.lld --version | head -n 1)"
+export KBUILD_COMPILER_STRING="$CLANG_VER"
+DATE=$(date +"%F-%S")
 START=$(date +"%s")
-
-# Java
-command -v java > /dev/null 2>&1
+PATH=${ClangPath}/bin:${GCCaPath}/bin:${GCCbPath}/bin:${PATH}
 
 # Telegram
 export BOT_MSG_URL="https://api.telegram.org/bot$TG_TOKEN/sendMessage"
 
-# Telegram messaging
 tg_post_msg() {
   curl -s -X POST "$BOT_MSG_URL" -d chat_id="$TG_CHAT_ID" \
-    -d "disable_web_page_preview=true" \
-    -d "parse_mode=html" \
-    -d text="$1"
+  -d "disable_web_page_preview=true" \
+  -d "parse_mode=html" \
+  -d text="$1"
+
 }
-# Compiler
+
+# Compile
 compile(){
 cd ${KERNEL_ROOTDIR}
-msg "|| Cooking kernel. . . ||"
 export HASH_HEAD=$(git rev-parse --short HEAD)
 export COMMIT_HEAD=$(git log --oneline -1)
 make -j$(nproc) O=out ARCH=arm64 asus/X00TD_defconfig
 make -j$(nproc) ARCH=arm64 SUBARCH=arm64 O=out \
-     PATH=$ClangPath/bin:${PATH} \
-     CC=clang \
-     CROSS_COMPILE=aarch64-linux-gnu- \
-     HOSTCC=clang \
-     HOSTCXX=clang++ ${ClangMoreStrings}
+    LD_LIBRARY_PATH="${ClangPath}/lib64:${LD_LIBRARY_PATH}" \
+    CC=${ClangPath}/bin/clang \
+    NM=${ClangPath}/bin/llvm-nm \
+    CXX=${ClangPath}/bin/clang++ \
+    AR=${ClangPath}/bin/llvm-ar \
+    STRIP=${ClangPath}/bin/llvm-strip \
+    OBJCOPY=${ClangPath}/bin/llvm-objcopy \
+    OBJDUMP=${ClangPath}/bin/llvm-objdump \
+    OBJSIZE=${ClangPath}/bin/llvm-size \
+    READELF=${ClangPath}/bin/llvm-readelf \
+    CROSS_COMPILE=aarch64-linux-android- \
+    CROSS_COMPILE_ARM32=arm-linux-androideabi- \
+    CLANG_TRIPLE=aarch64-linux-gnu- \
+    HOSTAR=${ClangPath}/bin/llvm-ar \
+    HOSTCC=${ClangPath}/bin/clang \
+    HOSTCXX=${ClangPath}/bin/clang++
 
    if ! [ -a "$IMAGE" ]; then
 	finerr
 	exit 1
    fi
-
-   msg "|| Cloning AnyKernel ||"
-   git clone https://github.com/Tiktodz/AnyKernel3 -b hmp AnyKernel
-   cp $IMAGE AnyKernel
+  git clone https://github.com/Tiktodz/AnyKernel3 -b main AnyKernel
+	cp $IMAGE AnyKernel
 }
-# Push kernel to telegram
+# Push kernel to channel
 function push() {
     cd AnyKernel
-    curl -F document="@$ZIP_FINAL.zip" "https://api.telegram.org/bot$TG_TOKEN/sendDocument" \
+    ZIP=$(echo *.zip)
+    curl -F document=@$ZIP "https://api.telegram.org/bot$TG_TOKEN/sendDocument" \
         -F chat_id="$TG_CHAT_ID" \
         -F "disable_web_page_preview=true" \
         -F "parse_mode=html" \
-        -F caption="🔐<b>Build Done</b>
-        - <code>$((DIFF / 60)) minute(s) $((DIFF % 60)) second(s)... </code>
-
-        <b>📅 Build Date: </b>
-        -<code>$DATE</code>
-
-        <b>🐧 Linux Version: </b>
-        -<code>$VERSION</code>
-
-         <b>💿 Compiler: </b>
-        -<code>$KBUILD_COMPILER_STRING</code>
-
-        <b>📱 Device: </b>
-        -<code>$DEVICE_CODENAME($MANUFACTURERINFO)</code>
-
-        <b>🆑 Changelog: </b>
-        - <code>%0A<a href='$CL_URL'>Here</a></code>
-        <b></b>
-        #$KERNELNAME #$CODENAME #$VARIANT"
+        -F caption="Compile took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>$DEVICE_CODENAME</b> | <b>${KBUILD_COMPILER_STRING}</b>"
 }
-# Find Error
+# Fin Error
 function finerr() {
     curl -s -X POST "https://api.telegram.org/bot$TG_TOKEN/sendMessage" \
         -d chat_id="$TG_CHAT_ID" \
         -d "disable_web_page_preview=true" \
         -d "parse_mode=markdown" \
-        -d text="❌ Tetap menyerah...Pasti bisa!!!"
+        -d text="I'm tired of compiling kernels,And I choose to give up...please give me motivation"
     exit 1
 }
+
 # Zipping
 function zipping() {
     cd AnyKernel || exit 1
-
-    zip -r9 $KERNELNAME-$CODENAME-$VARIANT-$BASE-"$DATE" * -x .git README.md anykernel.sh .gitignore zipsigner* *.zip
-
-    ZIP_FINAL="$KERNELNAME-$CODENAME-$VARIANT-$BASE-$DATE"
-
-    msg "|| Signing Zip ||"
-    tg_post_msg "<code>🔑 Signing Zip file with AOSP keys..</code>"
-
-    curl -sLo zipsigner-3.0.jar https://github.com/Magisk-Modules-Repo/zipsigner/raw/master/bin/zipsigner-3.0-dexed.jar
-    java -jar zipsigner-3.0.jar "$ZIP_FINAL".zip "$ZIP_FINAL"-signed.zip
-    ZIP_FINAL="$ZIP_FINAL-signed"
+    zip -r9 $KERNELNAME-HMP-$DEVICE_CODENAME-4.19.300.zip *
     cd ..
 }
-
 compile
 zipping
 END=$(date +"%s")
